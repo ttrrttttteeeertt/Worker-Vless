e0ba52239e85227dd8ebe64d29c4a69798579092
ttrrttttteeeertt <167129917+ttrrttttteeeertt@users.noreply.github.com>
2024-07-01 22:37:26 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -10,7 +10,7 @@ let userID = 'ada32f6a-707a-46fd-80f1-340e825280bb';
 const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
-let dohURL = 'https://dns.shecan.ir'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
+let dohURL = 'https://free.shecan.ir'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
 // v2board api environment variables
 let nodeId = ''; // 1
